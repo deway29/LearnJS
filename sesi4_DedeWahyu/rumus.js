84ed22666b7e658de4971d676ff9e0fde4daf7e7
deway29 <dedewahyu1996@gmail.com>
2025-12-06 18:17:00 +0700

    Upload sesi4_DedeWahyu

---
+++ sesi4_DedeWahyu/rumus.js
@@ -0,0 +1,19 @@
+// rumus.js
+export function kalkulator(angka1, angka2, operator) {
+  switch (operator) {
+    case "+":
+      return angka1 + angka2;
+
+    case "-":
+      return angka1 - angka2;
+
+    case "/":
+      return angka1 / angka2;
+
+    case "*":
+      return angka1 * angka2;
+
+    default:
+      return "Operator tidak valid!";
+  }
+}
